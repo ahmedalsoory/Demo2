99703ad1f9b97e533dc4f4b14c9153c7cb132940
ahmed <Ahmedalsoory123456@gmail.com>
2026-09-12 19:34:51 +0200

    update message

--- ConsoleApp96/grand.cs
+++ ConsoleApp96/grand.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp96
     {
         void print()
         {
-            Console.WriteLine("new verdasdason");
+            Console.WriteLine("new verdasdason"); 
+        //https://github.com/dotnet/AspNetCore.Docs.git
         }
         void printStatus()
         {
---
+++ ConsoleApp96/nores.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp96
+{
+    internal class nores
+    {
+        void ptiny()
+        {
+            Console.WriteLine("print");
+        }
+    }
+}
